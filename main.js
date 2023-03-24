9b820792196aa101d570891243c6858848eeb6c9
NotZERO <76597601+TechnoWolf7671@users.noreply.github.com>
2023-03-24 10:49:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-let menuDiv = document.createElement('div');
-menuDiv.id = 'kryoMenu';
-let menuTitle = document.createElement('h1');
-menuTitle.innerText = 'Kryo Menu';
-menuDiv.appendChild(menuTitle);
-let iReadyButton = document.createElement('button');
-iReadyButton.innerText = 'Iready Menu';
-menuDiv.appendChild(iReadyButton);
-let blooketButton = document.createElement('button');
-blooketButton.innerText = 'Blooket Menu';
-menuDiv.appendChild(blooketButton);
-let akenaxButton = document.createElement('button');
-akenaxButton.innerText = 'Akenax Loader';
-menuDiv.appendChild(akenaxButton);
-let closeButton = document.createElement('button');
-closeButton.innerText = 'Close';
-menuDiv.appendChild(closeButton);
-document.body.appendChild(menuDiv);
-closeButton.addEventListener('click', () => {
-  menuDiv.remove();
-});
-iReadyButton.addEventListener('click', () => {
-  let script = document.createElement('script');
-  script.src = 'https://raw.githubusercontent.com/username/repo/master/file.js';
-  document.body.appendChild(script);
-})
-blooketButton.addEventListener('click', () => {
-  let script = document.createElement('script');
-  script.src = 'https://raw.githubusercontent.com/username/repo/master/file.js';
-  document.body.appendChild(script);
-});
-akenaxButton.addEventListener('click', () => {
-  let script = document.createElement('script');
-  script.src = 'https://raw.githubusercontent.com/username/repo/master/file.js';
-  document.body.appendChild(script);
-});
+// Create the menu
+let menu = document.createElement('div');
+menu.style.zIndex = '9999';
+
+// Create the buttons
+let btn1 = document.createElement('button');
+btn1.innerHTML = 'Button 1';
+btn1.onclick = () => execute('https://raw.githubusercontent.com/...');
+
+let btn2 = document.createElement('button');
+btn2.innerHTML = 'Button 2';
+btn2.onclick = () => execute('https://raw.githubusercontent.com/...');
+
+let btn3 = document.createElement('button');
+btn3.innerHTML = 'Button 3';
+btn3.onclick = () => execute('https://raw.githubusercontent.com/...');
+
+let btn4 = document.createElement('button');
+btn4.innerHTML = 'Close';
+btn4.onclick = () => menu.remove();
+
+// Append the buttons to the menu
+menu.appendChild(btn1);
+menu.appendChild(btn2);
+menu.appendChild(btn3);
+menu.appendChild(btn4);
+
+// Append the menu to the document
+document.body.appendChild(menu);
+
+// Function to execute the code from the URL
+function execute(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(code => {
+      eval(code);
+    });
